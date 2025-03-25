4c6fff6d3b9981bc1103fbc090d97610e9881c88
squidworlds <157069656+squidworlds@users.noreply.github.com>
2025-03-25 15:20:21 +0000

    loading csv files into draft file

---
+++ draft.R
@@ -0,0 +1,2 @@
+demand <- read.csv("C:\\Users\\Saioa\\OneDrive - University of Edinburgh\\y3s2\\StatComp\\Project02\\SCS_demand_modelling.csv")
+hourly_temp <- read.csv("C:\\Users\\Saioa\\OneDrive - University of Edinburgh\\y3s2\\StatComp\\Project02\\SCS_hourly_temp.csv")
