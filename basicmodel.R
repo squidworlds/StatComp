fc3e06df69daf26e433497927b83d23922f81c58
squidworlds <157069656+squidworlds@users.noreply.github.com>
2025-03-27 17:13:40 +0000

    correcting typo

--- basicmodel.R
+++ basicmodel.R
@@ -1,7 +1,7 @@
 ### loading the csv files as data.frames
 
 demand <- read.csv("C:\\Users\\Saioa\\OneDrive - University of Edinburgh\\y3s2\\StatComp\\Project02\\SCS_demand_modelling.csv")
-houraly_temp <- read.csv("C:\\Users\\Saioa\\OneDrive - University of Edinburgh\\y3s2\\StatComp\\Project02\\SCS_hourly_temp.csv")
+hourly_temp <- read.csv("C:\\Users\\Saioa\\OneDrive - University of Edinburgh\\y3s2\\StatComp\\Project02\\SCS_hourly_temp.csv")
 
 ### loading packages
 library(tidyverse)
