a774c8394bd340055194b4bab15fe7587a53d8db
squidworlds <157069656+squidworlds@users.noreply.github.com>
2025-03-27 17:09:25 +0000

    checking model for average temperature

--- basicmodel.R
+++ basicmodel.R
@@ -1,30 +1,13 @@
-## loading the csv files as data.frames
+### loading the csv files as data.frames
 
 demand <- read.csv("C:\\Users\\Saioa\\OneDrive - University of Edinburgh\\y3s2\\StatComp\\Project02\\SCS_demand_modelling.csv")
-hourly_temp <- read.csv("C:\\Users\\Saioa\\OneDrive - University of Edinburgh\\y3s2\\StatComp\\Project02\\SCS_hourly_temp.csv")
+houraly_temp <- read.csv("C:\\Users\\Saioa\\OneDrive - University of Edinburgh\\y3s2\\StatComp\\Project02\\SCS_hourly_temp.csv")
 
-## loading packages
+### loading packages
 library(tidyverse)
 theme_set(theme_bw())
 
-## loading the data
-head(demand)
-head(hourly_temp)
-
-## reorganise and make into one dataframe
-# we use the hourly_temp dataframe to find the mean temperature per day
-hourly_temp <- separate(hourly_temp, Date, c("Date", "Time"), sep = " ")
-hourly_temp$Date <- strptime(as.character(hourly_temp$Date), "%d/%m/%Y")
-hourly_temp$Date <- format(hourly_temp$Date, "%Y-%m-%d")
-
-daily_mean_temp <- hourly_temp 
-
-
-#data1 <- left_join(x = demand, y = hourly_temp)
-
-
-## check the new dataframe
-head(data1)
+### creating functions
 
 ## prediction function
 predicting <- function(lm, data){
@@ -71,14 +54,48 @@ plotting <- function(prediction, data, predictor){
   
 }
 
+### loading the data
+head(demand)
+head(hourly_temp)
+
+## joining dataframes to take mean daily temp
+
+# formatting date in the hourly_temp dataframe correctly
+hourly_temp <- separate(hourly_temp, Date, c("Date", "Time"), sep = " ")
+hourly_temp$Date <- strptime(as.character(hourly_temp$Date), "%d/%m/%Y")
+hourly_temp$Date <- format(hourly_temp$Date, "%Y-%m-%d")
+
+# create a dataframe with mean daily temperature
+mean_daily_temp <- hourly_temp %>%
+  group_by(Date) %>%
+  summarise(avg_daily_temp = mean(temp))
+head(mean_daily_temp)
+
+# join the two dataframes together by Date
+data1 <- left_join(x = demand, y = mean_daily_temp)
+head(data1)
+
+### applying our functions to the data
+
+## creating models
 lm_basic <- lm(demand_gross ~ wind + solar_S + temp + wdayindex + monthindex, data = demand)
 lm_year <- lm(demand_gross ~ wind + solar_S + temp + wdayindex + monthindex + year, data = demand)
+lm_avgtemp <- lm(demand_gross ~ avg_daily_temp, data = data1)
 
 est_basic <- predicting(lm_basic, demand)
 est_year <- predicting(lm_year, demand)
+est_avgtemp <- predicting(lm_avgtemp, data1)
 
 pp_basic <- plotting(est_basic, demand, "basic predictors")
 pp_basic
 
 pp_year <- plotting(est_year, demand, "year")
 pp_year
+
+pp_avgtemp <- plotting(est_avgtemp, data1, "average daily temperature")
+pp_avgtemp
+
+
+## look at R^2 values and adjusted R^2, MSE
+
+## split data into weekdays and weekends
