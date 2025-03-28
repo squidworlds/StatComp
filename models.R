e2ed531296d065c32902b3f370298af24b808a39
ellapark24 <114227064+ellapark24@users.noreply.github.com>
2025-03-28 12:17:29 +0000

    additional models including year cubed

--- models.R
+++ models.R
@@ -2,6 +2,8 @@ m_0 <- lm(demand_gross ~ wind + solar_S + temp + factor(wdayindex) + factor(mont
 
 m_0_year <- lm(demand_gross ~ wind + solar_S + temp + factor(wdayindex) + factor(monthindex) + year, data = demand)
 
+m_0_year_cubed <- lm(demand_gross ~ wind + solar_S + temp + factor(wdayindex) + factor(monthindex) + poly(year, 3), data = demand)
+
 m_0_year_to <- lm(demand_gross ~ wind + solar_S + TO + factor(wdayindex) + factor(monthindex) + year, data = demand)
 
 m_0_year_te <- lm(demand_gross ~ wind + solar_S + TE + factor(wdayindex) + factor(monthindex) + year, data = demand)
@@ -12,4 +14,6 @@ m_0_year_to_squared <- lm(demand_gross ~ (wind + solar_S + TO + factor(wdayindex
 
 m_0_year_te_squared <- lm(demand_gross ~ (wind + solar_S + TE + factor(wdayindex) + factor(monthindex) + year)^2, data = demand)
 
-m_0_year_cubed_te <- lm(demand_gross ~ wind + solar_S + TE + factor(wdayindex) + factor(monthindex) + year^3, data = demand)
+m_0_year_cubed_te <- lm(demand_gross ~ wind + solar_S + TE + factor(wdayindex) + factor(monthindex) + poly(year,3), data = demand)
+
+m_0_year_cubed_te_squared <- lm(demand_gross ~ (wind + solar_S + TE + factor(wdayindex) + factor(monthindex) + poly(year, 3))^2, data = demand)
