44966a1a0274824925e96ec02d58b731084ee32d
KieranM56 <156918301+KieranM56@users.noreply.github.com>
2025-03-28 13:17:18 +0000

    Current state of affairs

--- KieransDraft.R
+++ KieransDraft.R
@@ -1,5 +1,4 @@
 demand <- read.csv("C:\\Users\\kiera\\OneDrive - University of Edinburgh\\Stats Project 2\\Project02\\SCS_demand_modelling.csv")
-# hourly_temp <- read.csv("C:\\Users\\kiera\\OneDrive - University of Edinburgh\\Stats Project 2\\Project02\\SCS_hourly_temp.csv")
 
 ## loading packages
 library(tidyverse)
@@ -7,45 +6,12 @@ theme_set(theme_bw())
 
 ## loading the data
 head(demand)
-# head(hourly_temp)
 
-## reorganise and make into one dataframe
-# hourly_temp <- separate(hourly_temp, Date, c("Date", "Time"), sep = " ")
-# hourly_temp$Date <- strptime(as.character(hourly_temp$Date), "%d/%m/%Y")
-# hourly_temp$Date <- format(hourly_temp$Date, "%Y-%m-%d")
+
 data1 <- demand
-
-## check the new dataframe
+data1$wdayindex <- as.factor(data1$wdayindex)
+data1$monthindex <- as.factor(data1$monthindex)
 head(data1)
-
-## implementing the basic model
-# lm_basic <- lm(demand_gross ~ 1 + wind + solar_S + temp + wdayindex + monthindex, data = data1)
-
-
-# Function to compute error metrics
-# compute_predictive_scores <- function(df) {
-#   df %>%
-#     mutate(
-#       se = (.[[target_var]] - mean_pred)^2,
-#       ds = (.[[target_var]] - mean_pred)^2 / sd_pred^2 + 2 * log(sd_pred),
-#       mae = abs(.[[target_var]] - mean_pred),
-#       rae = abs(.[[target_var]] - mean_pred) / abs(.[[target_var]]),
-#       sr = (.[[target_var]] - mean_pred) / sd_pred,
-#       log_score = -0.5 * log(2 * pi * sd_pred^2) - se / (2 * sd_pred^2)
-#     ) %>%
-#     group_by(monthindex) %>%
-#     summarise(
-#       mean_se = mean(se, na.rm = TRUE),
-#       mean_ds = mean(ds, na.rm = TRUE),
-#       mean_mae = mean(mae, na.rm = TRUE),
-#       mean_rae = mean(rae, na.rm = TRUE),
-#       mean_sr = mean(sr, na.rm = TRUE),
-#       mean_log_score = mean(log_score, na.rm = TRUE),
-#       .groups = "drop"
-#     )
-# }
-
-  
 
 # Function to perform LOOCV for a given formula and dataset
 loocv_model <- function(data, formula) {
@@ -72,20 +38,21 @@ loocv_model <- function(data, formula) {
 estimate_model_loocv <- function(data) {
   
   # Define model formulas
-  formula_basic <- demand_gross ~ 1 + wind + solar_S + temp + wdayindex + monthindex
-  formula_year  <- demand_gross ~ 1 + wind + solar_S + temp + wdayindex + monthindex + year
+  formula_basic <- demand_gross ~ 1 + wind + solar_S + temp + factor(wdayindex) + factor(monthindex)
+  formula_year  <- demand_gross ~ 1 + wind + solar_S + temp + factor(wdayindex) + factor(monthindex) + year
+  formula_year_cubed <- demand_gross ~ wind + solar_S + TE + factor(wdayindex) + factor(monthindex) + poly(year, 3)^2
   
-  # Perform LOOCV for each model using correct logic
+# Perform LOOCV for each model using correct logic
   result_basic <- loocv_model(data, formula_basic)
   result_year  <- loocv_model(data, formula_year)
+  result_year_cubed <- loocv_model(data, formula_year_cubed)
   
   # Add model label
   result_basic$model <- "Basic"
   result_year$model  <- "Year"
-  
+  result_year_cubed$model <- "YearCubed"
   # Combine both results
-  results_all <- bind_rows(result_basic, result_year)
-  
+  results_all <- bind_rows(result_basic, result_year, result_year_cubed)
   # Compute predictive scores per month and per model
   monthly_scores <- results_all %>%
     group_by(model, monthindex) %>%
@@ -107,5 +74,4 @@ print(monthly_scores)
 
 formula_basic <- demand_gross ~ 1 + wind + solar_S + temp + wdayindex + monthindex
 result_basic <- loocv_model(data1, formula_basic)
-meanpred <- result_basic$mean_pred
-actual <- result_basic$actual
+
