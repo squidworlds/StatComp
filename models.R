c3f5ff9f2351001150c5a952ea99d05fde91e8bc
ellapark24 <114227064+ellapark24@users.noreply.github.com>
2025-03-29 12:43:27 +0000

    added model with no outliers

--- models.R
+++ models.R
@@ -20,3 +20,9 @@ m_0_year_cubed_te_squared <- lm(demand_gross ~ (wind + solar_S + TE + factor(wda
 
 best_no_factor <- lm(demand_gross ~ (wind + solar_S + TE + poly(wdayindex, 2) + monthindex + poly(year, 3))^2, data = demand)
 summary(best_no_factor)
+
+# Remove extreme outliers for month 11 and 2
+demand_no_outliers <- demand[demand$solar_S < 0.2,]
+best_no_outliers <- lm(demand_gross ~ wind + solar_S + TE + 
+                         poly(wdayindex, 2) + poly(monthindex, 3) + poly(year, 3), 
+                       data = demand_no_outliers)
